d2d1eac971aaf2d1505fe8123645f730336c1926
Susheela Choudry <susheelachoudry@gmail.com>
2019-09-06 22:05:06 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -7,25 +7,26 @@
 
 echo ##################################################################################
 #checking if brew available if not install it
-IS_BREW_AVAILABLE = brew help | grep 'command not found'
-[[ -z "$IS_BREW_AVAILABLE" ]] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+IS_BREW_AVAILABLE=brew help | grep 'command not found'
+[[ ! -z "$IS_BREW_AVAILABLE" ]] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 unset IS_BREW_AVAILABLE
 echo ##################################################################################
 
 
 echo ##################################################################################
-# "Removing node if any available in the system, Don't worry we will reinstall it again with nvm. It will manage node better than us."
-IS_NODE_AVAIALABLE = node -v
-[[ ! -z "$IS_NODE_AVAIALABLE"]] && brew uninstall node --ignore-dependencies
-unset IS_NODE_AVAIALABLE
+# "Removing node if any available in the system, Don't worry we will reinstall it again with nvm."
+#  "nvm will manage node better than us."
+if test  $(which node); then
+brew uninstall node --ignore-dependencies
+fi
 echo ##################################################################################
 
 
 echo ##################################################################################
 # "Installing nvm, node package manager"
-IS_NVM_AVAIALABLE = nvm --version
-[[ ! -z "$IS_NVM_AVAIALABLE"]] && brew install nvm
-unset IS_NVM_AVAIALABLE
+if ! test $(nvm --version); then
+brew install nvm
+fi
 echo ##################################################################################
 
 
@@ -41,26 +42,30 @@ which node
 # if you want some other version please use nvm use versionWhichYouWant,,,,,,  if it is not available in your system, install it by using nvm install node versionWhichYouWant.
 echo ##################################################################################
 
+echo ##################################################################################
+# "Installing nvm, node package manager"
+if ! test $(yarn --version); then
+brew install nvm
+fi
+echo ##################################################################################
+
 
 echo ##################################################################################
 # "Installing yarn, package manager"
-IS_YARN_AVAILABLE = yarn -v
+IS_YARN_AVAILABLE=yarn -v
 [[ -z "IS_YARN_AVAILABLE"]] && brew install yarn
-unset IS_YARN_AVAILABLE
 echo ##################################################################################
 
 
 echo ##################################################################################
 # "Installing sourcetree, if you are a command line person, remove below line and run the script." 
-IS_SOURCETREE_AVAILABLE = brew cask list | grep 'sourcetree'
+IS_SOURCETREE_AVAILABLE=brew cask list | grep 'sourcetree'
 [[ ! -z "$IS_SOURCETREE_AVAILABLE"]] && brew cask install sourcetree
-unset IS_SOURCETREE_AVAILABLE
 echo ##################################################################################
 
 
 echo ##################################################################################
 echo "Installing vscode"
-IS_VISUAL_STUDIO_AVAILABLE = brew cask list | grep 'visual-studio-code'
+IS_VISUAL_STUDIO_AVAILABLE=brew cask list | grep 'visual-studio-code'
 [[ ! -z "$IS_VISUAL_STUDIO_AVAILABLE"]] && brew cask install visual-studio-code
-unset IS_VISUAL_STUDIO_AVAILABLE
 echo ##################################################################################
